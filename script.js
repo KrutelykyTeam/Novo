c83aa66a29f268eeb4b63117ea1892cae387fe38
KrutelykyTeam <krutelykyteam@gmail.com>
2024-07-29 19:48:42 +0300

    Done

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-const offers = document.querySelectorAll('.offer');
-const offerlinks = document.querySelectorAll('.seemore')
-console.log(offerlinks, offers)
+const inputValue = document.getElementById('load')
+const saveButton = document.getElementById('save')
+const prev = document.getElementById('prev')
+const refresh = document.getElementById('reload')
+const reset = document.getElementById('reset')
 
-for (let i = 0; i < offers.length; i++) {
-    offers[i].addEventListener('mouseenter', ()=>{
-        offerlinks[i].classList.add('seemore-a')
-    })
-    
-    offers[i].addEventListener('mouseleave', ()=>{
-        offerlinks[i].classList.remove('seemore-a')
-    })
-}
+
+
+refresh.addEventListener('click', ()=>{
+    location.reload()
+})
+
+reset.addEventListener('click', ()=>{
+    localStorage.removeItem('img')
+    location.reload()
+})
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let savedImage = localStorage.getItem('img')
+    if(savedImage){
+        prev.src = savedImage;
+    }else{
+        prev.src = './img/2.jpg'
+    }
+})
+
+
+
+saveButton.addEventListener('click', () => {
+    const file = inputValue.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const base64String = e.target.result;
+            localStorage.setItem('img', base64String);
+            console.log('Изображение сохранено!');
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        alert('Выберите изображение');
+    }
+});
