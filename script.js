7e59f35b4fca71aa741fbeac1e66aa2bfc10ea03
Max Wheeler <142929902+MaxLakerCoding@users.noreply.github.com>
2024-07-29 18:34:59 +0300

    Update [unknown date]

---
+++ script.js
@@ -0,0 +1,13 @@
+const offers = document.querySelectorAll('.offer');
+const offerlinks = document.querySelectorAll('.seemore')
+console.log(offerlinks, offers)
+
+for (let i = 0; i < offers.length; i++) {
+    offers[i].addEventListener('mouseenter', ()=>{
+        offerlinks[i].classList.add('seemore-a')
+    })
+    
+    offers[i].addEventListener('mouseleave', ()=>{
+        offerlinks[i].classList.remove('seemore-a')
+    })
+}
